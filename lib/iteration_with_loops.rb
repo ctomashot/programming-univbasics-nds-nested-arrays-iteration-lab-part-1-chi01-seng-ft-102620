2675ced459fa50504056c74266e5f00fc6c30e52
James Nicholas Tomashot <ctomashot@gmail.com>
2020-10-16 02:05:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,5 +9,8 @@ def find_even_values(src)
       if src[row_index][element_index].even?
         p src[row_index][element_index]
       end
-      
+      element_index += 1
+    end
+    row_index += 1
+  end
 end
